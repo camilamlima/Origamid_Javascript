5c741d8ce9289ec91746c1fd87a3efae9d75afd5
Camila Oliveira Lima <angelmila1@gmail.com>
2023-01-10 21:57:52 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,36 @@
+// Declarar uma variável com o seu nome
+var nome = Camila;
+
+// Declarar uma variável com a sua idade
+var idade = 36;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "macarrao";
+
+// Declarar 5 variáveis diferentes sem valores
+var cachorro;
+gato;
+papagaio;
+sapo;
+macaco;
+
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+
+
+pocAccountInfo?.[selectedPocAccount]?.vendor?.vendorParticipation?.partnerStore === "BRF"
---
+++ script.js
@@ -0,0 +1,42 @@
+//Variáveis
+//Responsáveis por guardar dados na memória.
+//Inicia com a palavra var , let ou const
+
+var nome = "Camila";
+let idade = 28;
+const possuiFaculdade = true;
+
+// Evitam repetições
+// DRY (Don't repeat yourself)
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+// Sintaxe
+// Palavra chave var seguida do nome, sinal de igual e o valor.
+
+// Vírgula
+// Utilizei a vírgula para criar mais de uma variável, sem repetir a
+// palavra chave var.
+var nome = "André",
+  idade = 28,
+  possuiFaculdade = true;
+
+//   Sem valor
+//   Pode declarar ela e não atribuir valor inicialmente.
+var precoAplicativo;
+// retorna undefined
+
+// Nome
+// Os nomes podem iniciar com $, _, ou letras.
+// Podem conter números mas não iniciar com eles
+
+// Case sensitive
+// nome é diferente de Nome
+
+// Não utilizar palavras reservadas
+// https://www.w3schools.com/js/js_reserved.asp
+
+// Camel case
+// É comum nomearmos assim: abrirModal
