4708f3b7b9e92a0bc0bbf7d215a8cd6b90995e28
Camila Lima <camila.bruschetta@ab-inbev.com>
2023-01-19 21:09:38 -0300

    Continuando curso de javascript origamid

---
+++ Javascript_Iniciante/tiposDados/script.js
@@ -0,0 +1,12 @@
+// 7 tipos de Dados
+// Todos são primitivos exceto os objetos.
+
+var nome = "André"; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol(); // Symbol
+var novoObjeto = {}; // Object
+
+// Primitivos são dados imutáveis.
